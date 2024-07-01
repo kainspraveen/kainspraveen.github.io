53f89d791fea9b5a973aed358bb523edfe6b9fcb
Kains <39915748+kainspraveen@users.noreply.github.com>
2024-07-01 18:33:45 +0530

    added bi-lingual code

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ document.querySelectorAll('nav a').forEach(anchor => {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const langButton = document.getElementById('langButton');
+    const welcomeText = document.getElementById('welcomeText');
+
+    langButton.addEventListener('click', function () {
+        if (langButton.textContent === 'FR') {
+            // langButton.textContent = 'EN';
+            switchToFrench();
+        } else {
+            // langButton.textContent = 'FR';
+            switchToEnglish();
+        }
+    });
+
+    function switchToEnglish() {
+        document.documentElement.lang = 'en';
+        // welcomeText.textContent = 'Welcome to our website!';
+        window.location.href = './index-en.html';
+        console.log('Switched to English',window.location.href);
+    }
+
+    function switchToFrench() {
+        document.documentElement.lang = 'fr';
+        // welcomeText.textContent = 'Bienvenue sur notre site web!';
+        window.location.href = './index.html';
+        console.log('Switched to French',window.location.href);
+    }
+});
+
